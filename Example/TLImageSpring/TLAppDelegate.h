73b4f6cbb5f6feba82149f5b5df1fef2821ef36f
Andrew <anluanlu123@163.com>
2016-03-24 16:21:58 +0800

    Initial commit

---
+++ Example/TLImageSpring/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  TLImageSpring
+//
+//  Created by Andrew on 03/24/2016.
+//  Copyright (c) 2016 Andrew. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
