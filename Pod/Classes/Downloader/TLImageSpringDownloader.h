5bd7d9d78928b88fba55334252b353f45d18ac8d
Andrew <anluanlu123@163.com>
2016-03-24 18:03:21 +0800

    commit

--- Pod/Classes/Downloader/TLImageSpringDownloader.h
+++ Pod/Classes/Downloader/TLImageSpringDownloader.h
@@ -8,6 +8,43 @@
 
 #import <Foundation/Foundation.h>
 
+
+typedef NS_ENUM(NSInteger,TLImageSpringDownloaderExcutionOrder){
+    /**
+     *  默认实现，下载操作将会执行先进先出的顺序
+     */
+   TLImageSpringDownloaderFIFOExecutionOrder,
+    /**
+     *  下载操作将会执行进栈的方式（最先进去的，最后一个出来）
+     */
+   TLImageSpringDownloaderLIFOExecutionOrder
+};
+
 @interface TLImageSpringDownloader : NSObject
--(void)startDownload;
+
+
+/**
+ *  下载的超时时间(秒)
+ */
+@property (nonatomic) NSTimeInterval  downloadTimerOut;
+
+/**
+ *  下载操作的执行顺序
+ */
+@property (nonatomic) TLImageSpringDownloaderExcutionOrder excutionOrder;
+
+/**
+ *  单例的实现方法
+ *
+ *  @return TLImageSpringDownloader
+ */
++(TLImageSpringDownloader*)sharedInstance;
+
+/**
+ *  挂起当前正在下载的线程
+ *
+ *  @param flag 是否挂起线程
+ */
+-(void)suspendCurrentDownload:(BOOL)flag;
+
 @end
--- Pod/Classes/Downloader/TLImageSpringDownloader.m
+++ Pod/Classes/Downloader/TLImageSpringDownloader.m
@@ -9,10 +9,65 @@
 #import "TLImageSpringDownloader.h"
 #import "SDNetworkActivityIndicator.h"
 
+@interface TLImageSpringDownloader ()
+@property (nonatomic,strong)NSOperationQueue *downloadQueue;
+@property (nonatomic,strong)NSOperation *lastedOperation;
+
+@property (nonatomic,strong)NSMutableDictionary *HttpHeaders;
+
+/**
+ *  所有的下载操作在这个线程中完成
+ */
+@property (nonatomic,strong) dispatch_queue_t responseQueue;
+
+
+@end
+
+
 @implementation TLImageSpringDownloader
--(void)startDownload{
-    SDNetworkActivityIndicator *activitivity=[SDNetworkActivityIndicator sharedActivityIndicator];
-    
-    [activitivity startActivity];
+
++(TLImageSpringDownloader*)sharedInstance{
+    static dispatch_once_t once;
+    static id instance;
+    dispatch_once(&once, ^{
+        instance=[[self alloc] init];
+    });
+    return instance;
+}
+-(id)init{
+    self=[super init];
+    if(self){
+        _excutionOrder=TLImageSpringDownloaderFIFOExecutionOrder;
+        _downloadQueue=[[NSOperationQueue alloc]init];
+        //配置queue的最大并发操作数量
+        _downloadQueue.maxConcurrentOperationCount=10;
+       
+        //封装请求头 Accept指定客户端能够接收的内容类型
+        _HttpHeaders = [@{@"Accept": @"image/*;q=0.8"} mutableCopy];
+        
+     _responseQueue=dispatch_queue_create("com.tongli.tlimagespringDownloader", DISPATCH_QUEUE_CONCURRENT);
+        _downloadTimerOut=10;
+    }
+    return self;
+}
+
+/**
+ *  根据请求头的key返回对应的value值
+ *
+ *  @param field 请求头的key
+ *
+ *  @return 请求头对应的value
+ */
+-(NSString *)valueForHttpHeaderField:(NSString *)field{
+    return self.HttpHeaders[field];
+}
+
+/**
+ *  挂起线程
+ *
+ *  @param flag
+ */
+-(void)suspendCurrentDownload:(BOOL)flag{
+    [self.downloadQueue setSuspended:flag];
 }
 @end
